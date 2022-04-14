b6beb4c8bb7932ce1f16153289a557ee795f7ec9
taranovev <tar_ev@mail.ru>
2022-04-14 13:51:17 +0300

    Extend data validation for def register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,13 +1,14 @@
 from django.http import JsonResponse
 from django.templatetags.static import static
+from django.core.exceptions import ObjectDoesNotExist
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-
+import phonenumbers
 from .models import Product, Order, OrderQuantity
 
-import json
 
 def banners_list_api(request):
     # FIXME move data to db?
@@ -63,24 +64,51 @@ def product_list_api(request):
 
 @api_view(['POST'])
 def register_order(request):
-    key = 'products'
+    keys = ['products', 'firstname', 'phonenumber', 'address']
     order = request.data
-
-    if (order.get(key) != order.get(key) or 
-        not isinstance(order.get(key), list) or
-        not order.get(key)):
-        content = {'error': f'{key} is empty or string'}
-        return Response(content, status=status.HTTP_404_NOT_FOUND)
-
+    for key in keys:
+        if key == 'products':
+            if (order.get(key) != order.get(key) or 
+            not isinstance(order.get(key), list) or
+            not order.get(key)):
+                content = {'error': f'{key} is empty or string'}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
+        elif key == 'firstname':
+            if (order.get(key) != order.get(key) or 
+            not isinstance(order.get(key), str) or
+            not order.get(key)):
+                content = {'error': f'{key} is empty or not string'}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
+        elif key not in order.keys():
+                content = {'error': f'{key} is not in order'}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
+        else:
+            if order.get(key) != order.get(key):
+                content = {'error': f'{key} is empty'}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
+            elif key == 'phonenumber':
+                try:
+                    phonenumber_in_order = phonenumbers.parse(order.get(key),'RU')
+                    if not phonenumbers.is_valid_number(phonenumber_in_order):
+                        content = {'error': f'{key} is not valid'}
+                        return Response(content, status=status.HTTP_404_NOT_FOUND)
+                except:
+                    content = {'error': f'{key} is not valid'}
+                    return Response(content, status=status.HTTP_404_NOT_FOUND)
     else:
         create_order = Order.objects.create(first_name=order['firstname'],
                          last_name=order['lastname'],
                          address=order['address'],
                          phonenumber=order['phonenumber'],)
         for position_order in order['products']:
-            position = Product.objects.get(pk=position_order['product'])
-            OrderQuantity.objects.create(order=create_order,
+            try:
+                pk = position_order['product']
+                position = Product.objects.get(pk=pk)
+                OrderQuantity.objects.create(order=create_order,
                                  product=position,
                                  quantity=position_order['quantity'])
+            except ObjectDoesNotExist:
+                content = {'error': f'product {pk} invalid primary key'}
+                return Response(content, status=status.HTTP_404_NOT_FOUND)
 
         return Response(order)
